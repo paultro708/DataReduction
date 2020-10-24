5a6c1c89716fa4f430b068acd59be61e77476408
paultro708 <32535575+paultro708@users.noreply.github.com>
2020-10-24 16:44:37 +0200

    create DataPreparation class and used datasets

---
+++ modules/DataPreparation.py
@@ -0,0 +1,138 @@
+import numpy as np
+# import matplotlib.pyplot as plt
+import pandas as pd
+# from sklearn.cluster import KMeans
+# from sklearn.datasets import fetch_rcv1
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+def create_path_csv(folder, name):
+        """
+        Function creating string with path of dataset in csv format
+        :folder: folder name
+        :name: name of dataset
+        """
+        return folder + '\\' + name + '.csv'
+
+#path of folder with datasets in csv format
+datasets_folder = 'D:\Studia\inz\Repos\DataReduction\modules\datasets_csv' # "datasets_csv"
+
+#dictionary of names of datasets and paths
+dataset_path = {"iris": create_path_csv(datasets_folder, 'iris'),
+                "glass": create_path_csv(datasets_folder, 'glass'),
+                "letter": create_path_csv(datasets_folder, 'letter'),
+                "liver": create_path_csv(datasets_folder, 'liver'),
+                "pendigits": create_path_csv(datasets_folder, 'pendigits'),
+                "spambase": create_path_csv(datasets_folder, 'spambase'),
+                "segment": create_path_csv(datasets_folder, 'segment'),
+                "satimage": create_path_csv(datasets_folder, 'satimage'),
+                "yeast": create_path_csv(datasets_folder, 'yeast')
+                }
+
+
+class DataPreparation:
+    """
+    Class using for loading possible datasets and preparing for reducing algorithms and raports
+
+    Atributes:
+    :dataset_name: name of dataset
+    """
+    #name of dataset
+    dataset_name = 'iris'
+    #original loaded dataset as pandas data frame
+    dataset=1
+    
+    #all original data without labels
+    data_all=1
+    #labels for original data
+    data_label=1
+
+    #training data obtained from original dataset
+    data_all_train=1
+    #testing data obtained from original dataset
+    data_all_test=1
+    #labels of training data obtained from original dataset
+    data_label_train=1
+    #labels of training data obtained from original dataset
+    data_label_test = 1
+
+    #number of classes in dataset
+    n_classes=1
+
+    #array of fetures
+    features=[]
+
+    #name of column with class labels
+    class_col = 'class'
+
+    #dictionary of labels of classes and their numerical equivalent index
+    class_dict = dict()
+    
+
+    def __init__(self, name):
+        """
+        Initialize dataset
+
+        :name: name of dataset - one of: 
+                "iris", 
+                "glass", 
+                "letter", 
+                "liver", 
+                "pendigits", 
+                "spambase", 
+                "segment",
+                "satimage" or
+                "yeast"
+        """
+        self.dataset_name = name
+
+    
+    def load_dataset(self, class_col = 'class'):
+        """
+        Function loading dataset to pandas dataframe
+        """
+        self.class_col = class_col
+
+        # if path == 'default':
+        #     self.dataset = pd.read_csv(path)
+        # else:
+        self.dataset = pd.read_csv(dataset_path[self.dataset_name])
+
+    def prepare_dataset(self):
+        """
+        Function prepaing dataset for reduction
+        """
+        #if self.class_col: #column name
+        #create data frame with class labels
+        self.data_label = self.dataset[self.class_col]
+        #drop column with label 
+        self.data_all = self.dataset.drop(columns=self.class_col)
+
+        #map labels to 0-n indexes
+        self.class_dict = dict()
+        i=0
+        for label in set(self.data_label):
+            self.class_dict[label] = i
+            i+=1
+
+        #init array of features
+        self.features = self.data_all.columns
+
+        #convert to numpy array
+        self.data_all = np.array(self.data_all)
+        self.data_label = np.array(self.data_label)
+
+        #split data into train and test
+        self.data_all_train, self.data_all_test, self.data_label_train, self.data_label_test = train_test_split(self.data_all, self.data_label, test_size=0.3)
+        
+        #init number of classes
+        self.n_classes = len(set(self.data_label))
+
+
+data = DataPreparation("liver")
+data.load_dataset()
+data.prepare_dataset()
+
+print(data.features)
+print(data.class_dict)
+print(len(data.data_label))
