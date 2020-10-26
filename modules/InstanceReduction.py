b3455ea9ac1fc6a0eac8eb67f5df0b6fae43ce19
paultro708 <32535575+paultro708@users.noreply.github.com>
2020-10-26 18:10:39 +0100

    create abstracct class for instance reduction and clustering method

---
+++ modules/InstanceReduction.py
@@ -0,0 +1,36 @@
+from DataPreparation import DataPreparation
+from abc import ABCMeta, abstractmethod, ABC
+
+# #dictionary of names of reduction methods
+# dataset_path = {"iris": create_path_csv(datasets_folder, 'iris'),
+#                 "glass": create_path_csv(datasets_folder, 'glass'),
+#                 "letter": create_path_csv(datasets_folder, 'letter'),
+#                 "liver": create_path_csv(datasets_folder, 'liver'),
+#                 "pendigits": create_path_csv(datasets_folder, 'pendigits'),
+#                 "spambase": create_path_csv(datasets_folder, 'spambase'),
+#                 "segment": create_path_csv(datasets_folder, 'segment'),
+#                 "satimage": create_path_csv(datasets_folder, 'satimage'),
+#                 "yeast": create_path_csv(datasets_folder, 'yeast')
+#                 }
+
+class InstanceReduction(metaclass = ABCMeta):
+    """
+    Abstract class respresenting instance reduction method.
+    """   
+
+    def __init__(self, name, data: DataPreparation):
+        """
+        Initialize data for reduction and name of method
+        """
+        self.data = data
+        self.name = name
+
+    @abstractmethod
+    def reduce_instances(self):
+        """
+        Abstract method - main in class. Runs reduction algorithm.
+        """
+        pass
+
+
+
