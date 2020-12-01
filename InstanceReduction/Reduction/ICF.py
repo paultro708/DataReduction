a8f0d7ad018ed2899484a16650d4b4d36bc0a105
paultro708 <32535575+paultro708@users.noreply.github.com>
2020-12-01 14:52:21 +0100

    add max iteration

--- InstanceReduction/Reduction/ICF.py
+++ InstanceReduction/Reduction/ICF.py
@@ -7,41 +7,38 @@ from time import process_time
 
 class ICF(_Reduction):
 
-    def __init__(self, data: DataPreparation, k: int=5):
-        """Constructor of ENN class. Edited Nearest Neighbours algorith removes instances which label dusagrees with majority class in k nearest neighbours set.
+    def __init__(self, data: DataPreparation, max_iter: int = 3):
+        """Constructor of ICF class. Iterative Case Filtering algorithm first apply ENN algorith to reduce noise instances, than
+        iteratively removes other instances as follows. At each iteration, created coverage and reachable subsets. If reachable is 
+        smaller than coverage for instance, it is removed.
 
         Args:
             data (DataPreparation): instance of :DataPreparation class containing prepared dataset for ENN alhorithm application
-            k (int, optional): number of nearest neighbours takein into account during reduction. Defaults to 5.
-
+            max_iter (int): maximal number of performed interations
+            
         Raises:
             TypeError: when given parameter does not have apriopriate type
-            ValueError: when given :k value is less than 1 or greater than number of instances in reducting dataset.
         """
         if not isinstance(data, DataPreparation):
             raise TypeError('Atribute \'data\' must be DataPreparation instance')
         self.data = data
-        self.k = k
         self.red_data = []
         self.red_lab = []
+        self.max_iter = max_iter
 
-        if type(k) != int:
-            raise TypeError('k atribute must be integer value')
-        elif k < 1:
-            raise ValueError('k atribute must have value not less than 1')
-        elif k >= len(self.data.data_all_train):
-            raise ValueError('k atribute must have value less than number of instances in dataset')
+        if type(max_iter) != int:
+            raise TypeError('max_iter atribute must be integer value')
+        elif max_iter < 1:
+            raise ValueError('max_iter atribute must have value not less than 1')
 
 
-    def create_cov_reach(self, index:int)-> list:
+    def create_cov_reach(self, index:int):
         """Function creating coverage subset. Coverage subset contains nearest neighbours with same label, limited by first enemy.
         Without itself.
 
         Args:
-            index (int): index of instance creating coverage subset for
+            index (int): index of instance creating coverage subset for 
 
-        Returns:
-            list: list representing coverage subset
         """
         sort = self.graph.sort_id[index] 
         #index of nearest enemy
@@ -56,70 +53,75 @@ class ICF(_Reduction):
         for idx in self.coverage[index]:
                 self.reachable[idx].append(index)
 
+    def _init_params(self):
+        #init arrays for keeping instances
+        self.keep = np.ones((self.red_lab.shape))
+            
+        #recreate graph
+        self.graph.create_graph(self.red_data, self.red_lab)  
 
-    def reduce_instances(self, return_time = False):
-        print('Reducing the dataset using the ICF algorithm...')
-
-        #apply ENN algorith first
-        enn = ENN(self.data)
-        enn.reduce_instances()
-        #init reduced data and labels
-        self.red_data = enn.red_data
-        self.red_lab = enn.red_lab
-
-        #start time measurement
-        start = process_time()
-
-        #create graph for get information about neighbours and enemies
-        self.graph = _NNGraph()
-        self.graph.create_graph(self.red_data, self.red_lab)
-        
-        #do algorithm
-        progress = True
-        # #init arrays for keeping instances
-        # keep = np.ones((self.red_data.shape))
-
-        #init coverage and reachable
+        #init coverage and reachable subset
         self.coverage = []
         self.reachable = []
-        for i in range(len(self.data.data_label_train)):
+        for i in range(len(self.red_lab)):
             self.coverage.append([])
             self.reachable.append([])
 
         for idx, p in enumerate(self.red_lab):
             self.create_cov_reach(idx)
+    
+    def reduce_instances(self, return_time = False, return_n_iter = False):
+        print('Reducing the dataset using the ICF algorithm...')
+        #start time measurement
+        start = process_time()
 
-        while progress:
-            ###todo in def
-            #init arrays for keeping instances
-            keep = np.ones((self.red_lab.shape))
-            
-            #recreate graph
-            self.graph.create_graph(self.red_data, self.red_lab)
-            #init coverage and reachable subset
-            self.coverage = []
-            self.reachable = []
-            for i in range(len(self.red_lab)):
-                self.coverage.append([])
-                self.reachable.append([])
+        #apply ENN algorithm first
+        enn = ENN(self.data)
+        enn.reduce_instances()
 
-            for idx, p in enumerate(self.red_lab):
-                self.create_cov_reach(idx)
-            ######
+        #init reduced data and labels
+        self.red_data = enn.red_data
+        self.red_lab = enn.red_lab
+
+        #create graph to get information about neighbours and enemies
+        self.graph = _NNGraph()
+        self._init_params()
+
+        #do algorithm
+        progress = True
+        iteration = 0
+        while (progress and iteration < self.max_iter):
             progress = False
             for idx, p in enumerate(self.red_lab):
                 cov = self.coverage[idx]
                 reach = self.reachable[idx]
-                if len(reach) > len(cov):
-                    keep[idx] = 0
+                if (len(reach) > len(cov)):
+                    self.keep[idx] = 0
+                    progress = True
             
-            #get indexes where red_data is marked to remove
-            to_remove = np.where(keep == 1)[0]
-            self.red_data = np.delete(self.red_data, to_remove, axis=0)
-            self.red_lab = np.delete(self.red_lab, to_remove, axis=0)
-            
+            #get indexes where red_data is marked to remove and delete them
+            to_remove = np.where(self.keep == 1)[0]
+            removed_l = np.delete(self.red_lab, to_remove, axis=0)
+            #remove only if still will be all classes
+            if(len(set(removed_l))==self.data.n_classes):
+                self.red_data = np.delete(self.red_data, to_remove, axis=0)
+                self.red_lab = removed_l
+            else: 
+                break
+
+            #reinit all params: keep, coverage and reachable
+            #if it would be next iter
+            if (progress and iteration < self.max_iter):
+                self._init_params()
+            iteration +=1
+
         #end time measurement
         end = process_time()
+        time = end - start
 
-        if return_time:
-            return end - start
+        if return_time and return_n_iter:
+            return time, iteration+1
+        elif return_time:
+            return time
+        elif return_n_iter:
+            return iteration+1
--- example.py
+++ example.py
@@ -5,12 +5,14 @@ from InstanceReduction.Reduction.PCS import PCS
 from InstanceReduction.Reduction.MSS import MSS
 from InstanceReduction.Reduction.ICF import ICF
 
-data = DataPreparation('satimage')#filepath="D:\Studia\inz\datasets_csv\glass.csv", class_col='Type')#'iris')
+data = DataPreparation('iris')#filepath="D:\Studia\inz\datasets_csv\glass.csv", class_col='Type')#'iris')
 enn = ICF(data)
-print(enn.reduce_instances(return_time=True))
+print(enn.reduce_instances(return_time=True, return_n_iter=True))
 
 from InstanceReduction.Raport import Raport
 
 rap = Raport(data, enn.red_data, enn.red_lab)
-rap.draw_plots('Aattr', 'Battr', show = True, save = True) #,'sepallength','sepalwidth' 'Aattr', 'Battr' 'width', 'high'
+#rap.draw_plots('Aattr', 'Battr', show = True, save = True) #,'sepallength','sepalwidth' 'Aattr', 'Battr' 'width', 'high'
+
+rap.draw_plots('sepallength','sepalwidth', show = True, save = True)
 rap.print_raport(show_cf_matrix = False, save_cf_matrix= True) #c_type = 'all',show_cf_matrix = True, path ='', save_plots = False)
